b6908efcff3179f1ade7216ea1a84e958f0ddd4f
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-06 13:21:43 +0530

    Update WeeklyLogsPage.js

--- src/pages/WeeklyLogsPage.js
+++ src/pages/WeeklyLogsPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-//import './WeeklyLogsPage.css';
+import './WeeklyLogsPage.css';
 import API_BASE_URL from '../config/api';
 
 const API_URL = `${API_BASE_URL}/weeklylogs`;
