72249ef6dfd6b2e981838cc9607f6fc3ff5309c2
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-07 13:46:00 +0530

    Create ControlHubBackground.jsx

---
+++ components/backgrounds/ControlHubBackground.jsx
@@ -0,0 +1,12 @@
+// components/backgrounds/ControlHubBackground.jsx
+import React from "react";
+import "./controlHubBackground.css";
+
+export default function ControlHubBackground() {
+  return (
+    <div className="controlhub-bg">
+      <div className="grid-overlay" />
+      <div className="orbital-shapes" />
+    </div>
+  );
+}
