492ef49c6653ebfe5cb34878eeaa1532a6e0c279
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-07 18:14:16 +0530

    Create FileShareAnimation.js

---
+++ src/components/animations/FileShareAnimation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./fileShareAnimation.css";
+
+export default function FileShareAnimation() {
+  return (
+    <div className="file-share-animation-layer">
+      <div className="file-stream" />
+      <div className="file-stream reverse" />
+    </div>
+  );
+}
