50060368881b5388bdec8f37487d79d9813b582e
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-07 20:57:29 +0530

    Create ProjectAnimation.jsx

---
+++ src/components/animations/ProjectAnimation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ProjectAnimation.css';
+
+export default function ProjectAnimation() {
+  return (
+    <div className="project-animation-layer">
+      <div className="data-flow" />
+      <div className="matrix-particles" />
+    </div>
+  );
+}
