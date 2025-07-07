335cdf9c71a7717e49acdaf7ef32ed6771a75019
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-07 18:50:15 +0530

    Create FileShareAnimation.jsx

---
+++ src/components/animations/FileShareAnimation.jsx
@@ -0,0 +1,9 @@
+// src/components/animations/FileShareAnimation.jsx
+import React from 'react';
+import './fileShareAnimation.css';
+
+const FileShareAnimation = () => {
+  return <div className="fileshare-animation-layer" />;
+};
+
+export default FileShareAnimation;
