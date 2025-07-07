b99e10522437bfded97afe4412bfc1c26b43f61a
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-07 18:53:13 +0530

    Delete src/components/animations/FileShareAnimation.jsx

--- src/components/animations/FileShareAnimation.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/animations/FileShareAnimation.jsx
-import React from 'react';
-import './fileShareAnimation.css';
-
-const FileShareAnimation = () => {
-  return <div className="fileshare-animation-layer" />;
-};
-
-export default FileShareAnimation;
