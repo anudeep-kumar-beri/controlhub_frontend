7e409dceb8abc16e7e4ed6f19b74db9fd429301c
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-07 18:43:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import ErrorBoundary from './components/ErrorBoundary.js';
 import DashboardHome from './pages/DashboardHome.js';
 import SkillTrackerPage from './pages/SkillTracker/SkillTrackerPage';
 import SkillDetailPage from './pages/SkillTracker/SkillDetailPage';
-import FileShareBoardPage from './pages/FileShareBoardPage.js';
 import WeeklyLogsPage from './pages/WeeklyLogsPage.js';
 import JobTrackerPage from './pages/JobTrackerPage.js';
 import BookmarksPage from './pages/BookmarksPage.js';
 import QuickJournalPage from './pages/QuickJournalPage.js';
 import NotFoundPage from './pages/NotFoundPage.js';
+
+import ProjectsPage from './pages/ProjectsPage';
+import ProjectDetailPage from './pages/ProjectDetailPage';
 
 function App() {
   return (
@@ -18,12 +20,15 @@ function App() {
         <Route path="/" element={<DashboardHome />} />
         <Route path="/skill-tracker" element={<SkillTrackerPage />} />
         <Route path="/skills/:id" element={<SkillDetailPage />} />
-        <Route path="/file-share-board" element={<FileShareBoardPage />} />
-        <Route path="/file-share-board" element={<FileShareBoardPage />} />
         <Route path="/weekly-logs" element={<WeeklyLogsPage />} />
         <Route path="/job-tracker" element={<JobTrackerPage />} />
         <Route path="/bookmarks" element={<BookmarksPage />} />
         <Route path="/quick-journal" element={<QuickJournalPage />} />
+
+        {/* Projects system */}
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:id" element={<ProjectDetailPage />} />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </ErrorBoundary>
