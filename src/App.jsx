a67943ed512396b10051c0df339444f5e1122d25
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-08-07 20:35:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import QuickJournalPage from './pages/QuickJournalPage.js';
 import NotFoundPage from './pages/NotFoundPage.js';
 import ProjectsPage from './pages/ProjectsPage.js';
 import ProjectDetailPage from './pages/ProjectDetailPage.js';
-import FlowWorkspacePage from './pages/FlowWorkspacePage.js';
+import FlowWorkspacePage from './pages/FlowWorkspacePage.jsx';
 import './App.css'; // Import global styles
 
 function App() {
